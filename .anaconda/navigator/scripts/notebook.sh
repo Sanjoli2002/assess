a09bdcaa5a64b3799b87f2444b5b1d584967b904
Sanjoli2002 <sahusanjoli@gmail.com>
2024-02-16 18:36:51 +0530

    aDD

---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /home/knoldus/anaconda3/bin/activate /home/knoldus/anaconda3
+jupyter-notebook >/home/knoldus/.anaconda/navigator/scripts/notebook-out-1.txt 2>/home/knoldus/.anaconda/navigator/scripts/notebook-err-1.txt
